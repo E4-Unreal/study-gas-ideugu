a0514995c749b359e7d7bfbd20cbf83f4cb270ed
이정훈 <eu4ng97@gmail.com>
2024-04-03 02:33:43 +0900

    feat: 콤보 공격 기능 구현

--- Source/ArenaBattleGAS/GA/ABGA_Attack.h
+++ Source/ArenaBattleGAS/GA/ABGA_Attack.h
@@ -6,6 +6,7 @@
 #include "Abilities/GameplayAbility.h"
 #include "ABGA_Attack.generated.h"
 
+class UABComboActionData;
 class UCharacterMovementComponent;
 class AABCharacterBase;
 /**
@@ -19,16 +20,31 @@ class ARENABATTLEGAS_API UABGA_Attack : public UGameplayAbility
     TWeakObjectPtr<AABCharacterBase> AvatarCharacter;
     TWeakObjectPtr<UCharacterMovementComponent> CharacterMovement;
 
+protected:
+    UPROPERTY(EditAnywhere, Category = "Config")
+    TObjectPtr<UABComboActionData> CurrentComboData;
+
+    uint8 CurrentCombo = 0;
+    FTimerHandle ComboTimerHandle;
+    bool HasNextComboInput = false;
+
 public:
     UABGA_Attack();
 
     virtual void OnAvatarSet(const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilitySpec& Spec) override;
+    virtual void PreActivate(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, FOnGameplayAbilityEnded::FDelegate* OnGameplayAbilityEndedDelegate, const FGameplayEventData* TriggerEventData) override;
     virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
     virtual void InputPressed(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo) override;
     virtual void CancelAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateCancelAbility) override;
     virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
 
 protected:
+    /* 메서드 */
+    FName GetNextSection();
+    void StartComboTimer();
+    void CheckComboInput();
+
+    /* 이벤트 핸들링 메서드 */
     UFUNCTION()
     void OnCompleted_Event();
 
