3971ab132f6be4f2826a94136d198e18efcb9347
이정훈 <eu4ng97@gmail.com>
2024-04-02 20:09:54 +0900

    feat: 분수대 회전 기능 구현 (액터)

---
+++ Source/ArenaBattleGAS/Prop/ABGASFountain.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ABGASFountain.h"
+
+#include "GameFramework/RotatingMovementComponent.h"
+
+AABGASFountain::AABGASFountain()
+{
+    RotatingMovement = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("RotatingMovement"));
+    ActionPeriod = 3.f;
+}
+
+void AABGASFountain::PostInitializeComponents()
+{
+    Super::PostInitializeComponents();
+
+    RotatingMovement->bAutoActivate = false;
+    RotatingMovement->Deactivate();
+}
+
+void AABGASFountain::BeginPlay()
+{
+    Super::BeginPlay();
+
+    GetWorldTimerManager().SetTimer(ActionTimer, this, &ThisClass::TimerAction, ActionPeriod, true, 0.f);
+}
+
+void AABGASFountain::TimerAction()
+{
+    if(!RotatingMovement->IsActive())
+        RotatingMovement->Activate(true);
+    else
+        RotatingMovement->Deactivate();
+}
---
+++ Source/ArenaBattleGAS/Prop/ABGASFountain.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Prop/ABFountain.h"
+#include "ABGASFountain.generated.h"
+
+class URotatingMovementComponent;
+
+UCLASS()
+class ARENABATTLEGAS_API AABGASFountain : public AABFountain
+{
+    GENERATED_BODY()
+
+protected:
+    UPROPERTY(VisibleAnywhere, Category = "Component")
+    TObjectPtr<URotatingMovementComponent> RotatingMovement;
+
+    UPROPERTY(EditAnywhere, Category = "Config")
+    float ActionPeriod;
+
+    FTimerHandle ActionTimer;
+
+public:
+    AABGASFountain();
+
+protected:
+    /* Actor */
+    virtual void PostInitializeComponents() override;
+    virtual void BeginPlay() override;
+
+    virtual void TimerAction();
+};
