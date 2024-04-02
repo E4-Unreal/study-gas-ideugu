76c89ab01dd7ad6a66b1930caa8828e7d0d8e06f
이정훈 <eu4ng97@gmail.com>
2024-04-03 03:48:44 +0900

    feat: 콤보 어택 히트 판정 타이밍 설정을 위한 애님 노티파이 클래스 및 히트 판정 어빌리티 클래스 작성

---
+++ Source/ArenaBattleGAS/Animation/AnimNotify_GASAttackHitCheck.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotify_GASAttackHitCheck.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+
+UAnimNotify_GASAttackHitCheck::UAnimNotify_GASAttackHitCheck()
+{
+}
+
+FString UAnimNotify_GASAttackHitCheck::GetNotifyName_Implementation() const
+{
+    return TEXT("GASAttackHitCheck");
+}
+
+void UAnimNotify_GASAttackHitCheck::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
+    const FAnimNotifyEventReference& EventReference)
+{
+    Super::Notify(MeshComp, Animation, EventReference);
+
+    if(MeshComp)
+    {
+        if(AActor* OwnerActor = MeshComp->GetOwner())
+        {
+            FGameplayEventData PayloadData;
+            UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(OwnerActor, TriggerTag, PayloadData);
+        }
+    }
+}
---
+++ Source/ArenaBattleGAS/GA/ABGA_AttackHitCheck.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABGA_AttackHitCheck.h"
+
+UABGA_AttackHitCheck::UABGA_AttackHitCheck()
+{
+    InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+}
+
+void UABGA_AttackHitCheck::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+    const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+    const FGameplayEventData* TriggerEventData)
+{
+    Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+    UE_LOG(LogTemp, Error, TEXT("HitCheck"))
+
+    bool bReplicatedEndAbility = true;
+    bool bWasCancelled = true;
+    EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, bReplicatedEndAbility, bWasCancelled);
+}
