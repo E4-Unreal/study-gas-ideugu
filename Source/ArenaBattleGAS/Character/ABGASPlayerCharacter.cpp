0458e394c0ccd569bc294748d6cb85912195a78e
이정훈 <eu4ng97@gmail.com>
2024-04-02 23:17:06 +0900

    feat: ASC가 포함된 PlayerState 클래스 작성 및 점프 액션과 점프 어빌리티 바인딩

---
+++ Source/ArenaBattleGAS/Character/ABGASPlayerCharacter.cpp
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ABGASPlayerCharacter.h"
+
+#include "AbilitySystemComponent.h"
+#include "EnhancedInputComponent.h"
+#include "Player/ABGASPlayerState.h"
+
+void AABGASPlayerCharacter::PossessedBy(AController* NewController)
+{
+    Super::PossessedBy(NewController);
+
+    if(AABGASPlayerState* PS = GetPlayerState<AABGASPlayerState>())
+    {
+        // ASC 레퍼런스 주입
+        AbilitySystem = PS->GetAbilitySystemComponent();
+
+        // ASC 초기화
+        AbilitySystem->InitAbilityActorInfo(PS, this);
+
+        // 기본 GA 부여
+        int32 InputID = 0;
+        for(const auto& DefaultAbility : DefaultAbilities)
+        {
+            FGameplayAbilitySpec GameplayAbilitySpec = AbilitySystem->BuildAbilitySpecFromClass(DefaultAbility);
+            GameplayAbilitySpec.InputID = InputID;
+            AbilitySystem->GiveAbility(GameplayAbilitySpec);
+
+            InputID++;
+        }
+    }
+}
+
+void AABGASPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    SetupGasInputComponent();
+}
+
+void AABGASPlayerCharacter::SetupGasInputComponent()
+{
+    if(AbilitySystem.IsValid() && IsValid(InputComponent))
+    {
+        UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+        EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Triggered, AbilitySystem.Get(), &UAbilitySystemComponent::PressInputID, 0);
+        EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, AbilitySystem.Get(), &UAbilitySystemComponent::ReleaseInputID, 0);
+    }
+}
---
+++ Source/ArenaBattleGAS/Character/ABGASPlayerCharacter.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "Character/ABCharacterPlayer.h"
+#include "ABGASPlayerCharacter.generated.h"
+
+class UAbilitySystemComponent;
+class UGameplayAbility;
+
+UCLASS()
+class ARENABATTLEGAS_API AABGASPlayerCharacter : public AABCharacterPlayer, public IAbilitySystemInterface
+{
+    GENERATED_BODY()
+
+    TWeakObjectPtr<UAbilitySystemComponent> AbilitySystem;
+
+protected:
+    UPROPERTY(EditAnywhere, Category = "Config")
+    TArray<TSubclassOf<UGameplayAbility>> DefaultAbilities;
+
+public:
+    virtual void PossessedBy(AController* NewController) override;
+    virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+protected:
+    void SetupGasInputComponent();
+
+public:
+    /* AbilitySystemInterface */
+    FORCEINLINE virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override { return AbilitySystem.Get(); }
+};
