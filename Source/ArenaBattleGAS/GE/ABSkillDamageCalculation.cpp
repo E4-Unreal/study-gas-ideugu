bc4360300cb533c4a14d35b26f7e5fa75814b4c5
이정훈 <eu4ng97@gmail.com>
2024-04-05 00:31:44 +0900

    feat: 거리에 따라 데미지를 다르게 적용하는 GameplayEffectExecutionCalculation 클래스 생성 및 적용

---
+++ Source/ArenaBattleGAS/GE/ABSkillDamageCalculation.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABSkillDamageCalculation.h"
+
+#include "AbilitySystemComponent.h"
+#include "Attribute/ABCharacterSkillAttributeSet.h"
+#include "Attribute/ABGASCharacterAttributeSet.h"
+
+void UABSkillDamageCalculation::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+                                                       FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+    Super::Execute_Implementation(ExecutionParams, OutExecutionOutput);
+
+    UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+    UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+    if(SourceASC && TargetASC)
+    {
+        AActor* SourceActor = SourceASC->GetAvatarActor();
+        AActor* TargetActor = TargetASC->GetAvatarActor();
+
+        if(SourceActor && TargetActor)
+        {
+            const float MaxDamageRange = SourceASC->GetNumericAttribute(UABCharacterSkillAttributeSet::GetSkillRangeAttribute());
+            const float MaxDamage = SourceASC->GetNumericAttributeBase(UABCharacterSkillAttributeSet::GetSkillRateAttribute());
+            const float Distance = FMath::Clamp(SourceActor->GetDistanceTo(TargetActor), 0.f, MaxDamageRange);
+            const float InvDamageRatio = 1.f - Distance / MaxDamageRange;
+            float Damage = InvDamageRatio * MaxDamage;
+
+            OutExecutionOutput.AddOutputModifier(FGameplayModifierEvaluatedData(UABGASCharacterAttributeSet::GetDamageAttribute(), EGameplayModOp::Additive, Damage));
+        }
+    }
+}
